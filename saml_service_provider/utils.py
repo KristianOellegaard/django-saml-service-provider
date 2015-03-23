9d2a7fa95f6a04b8b4343c6240aa8c48b5ffb7b1
Kristian Øllegaard <kristian@kristian.io>
2015-03-23 22:56:07 +0100

    support ELB or other load balancers

--- saml_service_provider/utils.py
+++ saml_service_provider/utils.py
@@ -2,7 +2,7 @@ def prepare_from_django_request(request):
     return {
         'http_host': request.META['HTTP_HOST'],
         'script_name': request.META['PATH_INFO'],
-        'server_port': request.META['SERVER_PORT'],
+        'server_port': request.META.get('HTTP_X_FORWARDED_PORT', request.META['SERVER_PORT']),
         'get_data': request.GET.copy(),
         'post_data': request.POST.copy()
     }
