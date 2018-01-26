d9c4b027a31403b60a77457932240c02f4b092a7
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-01-26 14:29:59 -0800

    Add initial tests

--- setup.py
+++ setup.py
@@ -1,6 +1,41 @@
-from setuptools import find_packages, setup
+from setuptools import Command, find_packages, setup
 
 from saml_service_provider import __version__ as version
+
+
+class TestCommand(Command):
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        import django
+        from django.conf import settings
+        from django.core.management import call_command
+
+        settings.configure(
+            DATABASES={
+                'default': {
+                    'NAME': ':memory:',
+                    'ENGINE': 'django.db.backends.sqlite3',
+                },
+            },
+            INSTALLED_APPS=(
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.sessions',
+            ),
+            MIDDLEWARE_CLASSES=('django.contrib.sessions.middleware.SessionMiddleware',),
+            ROOT_URLCONF='saml_service_provider.urls',
+            AUTHENTICATION_BACKENDS=['saml_service_provider.auth_backend.SAMLServiceProviderBackend']
+        )
+        django.setup()
+        call_command('test', 'saml_service_provider')
 
 
 setup(
@@ -18,6 +53,9 @@ setup(
         'Django >= 1.4',
         'python-saml',
     ],
+    tests_require=[
+        'mock',
+    ],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Framework :: Django",
@@ -25,5 +63,6 @@ setup(
         "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 2.7",
-    ]
+    ],
+    cmdclass={'test': TestCommand}
 )
