b86d7fb4438f048919015531517a742aa88d075f
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-01-30 13:59:07 -0800

    FIX: Update user attributes on sign-in

--- saml_service_provider/auth_backend.py
+++ saml_service_provider/auth_backend.py
@@ -3,20 +3,27 @@ from django.contrib.auth.models import User
 
 class SAMLServiceProviderBackend(object):
 
+    def update_attributes(self, user, attributes):
+        # Set name
+        user.first_name = attributes['First name'][0]
+        user.last_name = attributes['Last name'][0]
+        user.save()
+
     def authenticate(self, saml_authentication=None):
         if not saml_authentication:  # Using another authentication method
             return
 
         if saml_authentication.is_authenticated():
-            attributes = saml_authentication.get_attributes()
             try:
                 user = User.objects.get(username=saml_authentication.get_nameid())
             except User.DoesNotExist:
                 user = User(username=saml_authentication.get_nameid())
                 user.set_unusable_password()
-                user.first_name = attributes['First name'][0]
-                user.last_name = attributes['Last name'][0]
-                user.save()
+
+            # Set attributes (and create user, if not yet created)
+            attributes = saml_authentication.get_attributes()
+            self.update_attributes(user, attributes)
+
             return user
         return
 
