f213d96fdff4b508f526f94aae1b870d0f44ead1
Lucas Connors <lucas.revolutiontech@gmail.com>
2018-01-30 12:41:55 -0800

    Add trailing commas to multi-line dicts
    so that additions to the dict do not require changes
    to an irrelevant line

--- saml_service_provider/utils.py
+++ saml_service_provider/utils.py
@@ -4,5 +4,5 @@ def prepare_from_django_request(request):
         'script_name': request.META['PATH_INFO'],
         'server_port': request.META.get('HTTP_X_FORWARDED_PORT', request.META['SERVER_PORT']),
         'get_data': request.GET.copy(),
-        'post_data': request.POST.copy()
+        'post_data': request.POST.copy(),
     }
